2e1971b38485035f2618397d145b8b6b0e1f3b60
VPatel884 <vpatel884@gmail.com>
2023-09-01 09:57:01 +0000

    Created user menu

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 const readlineSync = require('readline-sync');
+
+function userMenu() {
+  console.log('Daily Task Manager');
+  console.log('1. Add task');
+  console.log('2. Tasks lists');
+  console.log('3. Tasks status');
+  console.log('4. Remove task');
+  console.log('5. Exit');
+}
