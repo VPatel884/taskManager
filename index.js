efbd52dd110e8084dccc1186adf0727577b2a3c6
VPatel884 <vpatel884@gmail.com>
2023-09-01 10:19:03 +0000

    Adding functions to user menu

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const readlineSync = require('readline-sync');
 
-const task = [];
+const tasks = [];
 
 function userMenu() {
   console.log('Daily Task Manager');
@@ -9,4 +9,18 @@ function userMenu() {
   console.log('3. Tasks status');
   console.log('4. Remove task');
   console.log('5. Exit');
+}
+
+function addTask(name,description) {
+  readlineSync.question('Enter task name: ');
+  readlineSync.question('Enter task description: ')
+  tasks.push({name,description});
+}
+
+function listOfTasks() {
+  for (task of tasks) {
+    console.log('Task: ', tasks.name);
+    console.log('Task description: ', tasks.description);
+    console.log('-------------')
+  }
 }
